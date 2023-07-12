20b06ab3aa3ac2462d323fb149c09b226f8dc851
Mauro <101263125+PeanutSouth@users.noreply.github.com>
2023-07-12 13:41:54 -0300

    BOT

--- Redes/src/ClientePrueba.java
+++ Redes/src/ClientePrueba.java
@@ -2,54 +2,19 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Scanner;
 
-public class ClientePrueba {
-    public String Uno(Socket socket, BufferedReader input,  PrintWriter output){
-        Scanner s=new Scanner(System.in);
-        ArrayList<String> topics=new ArrayList<String>();
-        int aux=1;
-        String message="";
-        output.println("Topics");
-        try {
-            while (input.ready()) {
-                message = input.readLine();
-                System.out.println(message);
-                topics.add(message);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        for(String st:topics){
-            System.out.println(st);
-        }
-        System.out.println("Que topic:");
-        String t=s.nextLine();
-        if(topics.contains(t)) {
-            for (int i = 0; i < topics.size(); i++) {
-                System.out.println(i + " = " + topics.get(i));
-            }
-            return topics.get(s.nextInt());
-        }
-        else{
-            System.out.println("No existe, creando...");
-            return t;
-        }
 
-    }
+public class ClientePrueba {
     public static void main(String[] args) {
         int aux=1;
         String topic="";
         String message=null;
         String serverAddress = "172.16.255.190";
-        Scanner s=new Scanner(System.in);
-        int serverPort = 4001;
-        ClientePrueba c=new ClientePrueba();
-
-
-
+        int serverPort = 4003;
 
         try {
             Socket socket = new Socket(serverAddress, serverPort);
@@ -57,61 +22,15 @@ public class ClientePrueba {
 
             BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
-
-
+            output.println("nombre:BOT_FECHA");
+            output.println("s:Fecha");
             while(true){
-                System.out.println("Que queres hacer:");
-                System.out.println("1=Suscribirse a un topic");
-                System.out.println("2=Desuscribirese de un topic");
-                System.out.println("3=Enviar mensaje");
-                System.out.println("4=Leer mensajes recibidos");
-                switch(s.nextInt()){
-                    case 1:
-                        output.println("s:"+c.Uno(socket, input, output));
-                        break;
-                    case 2:
-                        System.out.println("Que topic:");
-                        System.out.println("1=Clima");
-                        System.out.println("2=Fecha");
-                        switch (s.nextInt()){
-                            case 1:
-                                output.println("u:Clima");
-                                break;
-                            case 2:
-                                output.println("u:Fecha");
-                                break;
-                        }
-                        break;
-                    case 3:
-                        System.out.println("Escribir mensaje");
-                        s.nextLine(); // Se come el salto de linea
-                        String m=s.nextLine();
-                        System.out.println("Que topic:");
-                        System.out.println("1=Clima");
-                        System.out.println("2=Fecha");
-                        int t=s.nextInt();
-                        switch (t){
-                            case 1:
-                                output.println("m:Clima:"+m);
-                                break;
-                            case 2:
-                                output.println("m:Fecha:"+m);
-                                break;
-                        }
-                        break;
-                    case 4:
-                        System.out.println("Mensajes recibidos:");
-                        while (input.ready()) {
-                            message = input.readLine();
-                            if(aux==1) {
-                                System.out.println(message);
-                                aux=0;
-                            }
-                            else{
-                                aux=1;
-                            }
-                        }
-                        break;
+                while (input.ready()) {
+                    message = input.readLine();
+                    System.out.println("Mensaje: " + message);
+                    if(message.startsWith("Fecha:hora")){
+                        output.println("m:Fecha:"+LocalDateTime.now());
+                    }
                 }
                 Thread.sleep(1000);
             }
