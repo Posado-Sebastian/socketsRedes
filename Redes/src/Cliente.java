06c510f1139c3f50504296b3dfaaf18a394bcdd4
Mauro <101263125+PeanutSouth@users.noreply.github.com>
2023-07-12 13:07:59 -0300

    Add files via upload

--- Redes/src/Cliente.java
+++ Redes/src/Cliente.java
@@ -11,7 +11,7 @@ public class Cliente {
         int aux=1;
         String topic="";
         String message=null;
-        String serverAddress = "localhost";
+        String serverAddress = "172.16.255.190";
         Scanner s=new Scanner(System.in);
         int serverPort = 4001;
         boolean si=true;
